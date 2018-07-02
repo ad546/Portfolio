0f5b132de368215fe11a9467c504ea6289049383
ad546 <ad546github@gmail.com>
2018-07-02 09:27:37 -0500

    card flip working

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
+function cardFlip(num) {
+    if (num === 1){
+        document.getElementById("about").style.zIndex = "1";
+        document.getElementById("projects").style.zIndex = "0";
+        document.getElementById("contact").style.zIndex = "0";
+    }
+    else if (num === 2) {
+        document.getElementById("about").style.zIndex = "0";
+        document.getElementById("projects").style.zIndex = "1";
+        document.getElementById("contact").style.zIndex = "0";
+    }
+    else {
+        document.getElementById("about").style.zIndex = "0";
+        document.getElementById("projects").style.zIndex = "0";
+        document.getElementById("contact").style.zIndex = "1";
+    }
 
+    let element = document.getElementById("flip-c");
+    element.classList.toggle("flip");
+}
 
