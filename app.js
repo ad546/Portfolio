a9daf183ff3a7a7ae57b02a4201ac27f8f56914c
ad546 <ad546github@gmail.com>
2018-07-03 09:10:01 -0500

    card flip finalized

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
-function cardFlip(num) {
-    if (num === 1){
-        document.getElementById("about").style.zIndex = "1";
-        document.getElementById("projects").style.zIndex = "0";
-        document.getElementById("contact").style.zIndex = "0";
-    }
-    else if (num === 2) {
-        document.getElementById("about").style.zIndex = "0";
-        document.getElementById("projects").style.zIndex = "1";
-        document.getElementById("contact").style.zIndex = "0";
-    }
-    else {
-        document.getElementById("about").style.zIndex = "0";
-        document.getElementById("projects").style.zIndex = "0";
-        document.getElementById("contact").style.zIndex = "1";
-    }
-
-    let element = document.getElementById("flip-c");
+function leftFlip() {
+    let element = document.getElementById("flip-projects");
     element.classList.toggle("flip");
+    if(document.getElementById("flip-contact").classList.contains("horizontal-flip-x")) {
+        document.getElementById("flip-contact").classList.toggle("horizontal-flip-x");
+    }
+    if(document.getElementById("flip-about").classList.contains("horizontal-flip")) {
+        document.getElementById("flip-about").classList.toggle("horizontal-flip");
+    }
 }
+
+function topFlip() {
+    let element = document.getElementById("flip-contact");
+    element.classList.toggle("horizontal-flip-x");
+    if(document.getElementById("flip-about").classList.contains("horizontal-flip")) {
+        document.getElementById("flip-about").classList.toggle("horizontal-flip");
+    }
+    if(document.getElementById("flip-projects").classList.contains("flip")) {
+        document.getElementById("flip-projects").classList.toggle("flip");
+    }
+}
+
+function bottomFlip() {
+    let element = document.getElementById("flip-about");
+    element.classList.toggle("horizontal-flip");
+    if(document.getElementById("flip-contact").classList.contains("horizontal-flip-x")) {
+        document.getElementById("flip-contact").classList.toggle("horizontal-flip-x");
+    }
+    if(document.getElementById("flip-projects").classList.contains("flip")) {
+        document.getElementById("flip-projects").classList.toggle("flip");
+    }
+}
+    
+    
+
 
